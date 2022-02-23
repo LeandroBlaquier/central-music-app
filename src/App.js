e81b2b21a25f4b1b2df30fafce4944343ce7c6a5
SatouKz <alexander.ajalla404@gmail.com>
2022-02-23 16:53:34 -0300

    card advanced-sm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css';
+// import './App.css';
 import ProductCard from './components/pages/ProductCard'
 import Error404 from './components/pages/Error404'
 
