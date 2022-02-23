559a701f38f4443c28e8d8f20dcbf11fc6d9177b
SatouKz <alexander.ajalla404@gmail.com>
2022-02-22 22:46:47 -0300

    card-advanced

---
+++ src/assets/products.js
@@ -0,0 +1,11 @@
+import guitar from './images/guitar.png';
+import guitar2 from './images/guitar2.png';
+import guitar3 from './images/guitar3.png';
+
+
+const Pictures = [
+    {id:1, image : guitar , tittle: 'Brand Name', price: '$2,995',description: 'Product description here'},
+    {id:2, image : guitar2 , tittle: 'Brand Name', price: '$2,995',description: 'Product description here'},
+    {id:3, image : guitar3 , tittle: 'Brand Name', price: '$2,995',description: 'Product description here'},
+]
+export default Pictures;
