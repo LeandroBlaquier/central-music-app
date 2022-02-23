38f641282b5acf8b1dc778935e3f92177f6a6c15
SatouKz <alexander.ajalla404@gmail.com>
2022-02-23 19:49:55 -0300

    card/fixed/3

--- src/App-card.js
+++ src/App-card.js
@@ -6,7 +6,6 @@ import Error404 from './components/pages/Error404'
 function App() {
   return(
     <>
-<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path='/' element={<ProductCard/>}></Route>
@@ -16,12 +15,4 @@ function App() {
     </>
   )
 }
-=======
-    <h1>NASHE ratatatat</h1>
-    <h2>k tal</h2>
-    </>
-  )
-};
->>>>>>> be94f7d8bc9801bedeebe261b70571dd0f667474
-
 export default App;
