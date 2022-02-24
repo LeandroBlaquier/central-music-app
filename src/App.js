94542bf00b92de0dae3ed7c78a8c32ca18281876
SatouKz <alexander.ajalla404@gmail.com>
2022-02-23 21:50:10 -0300

    develop/fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import './App.css';
-import ProductCard from './components/pages/ProductCard'
-import Error404 from './components/pages/Error404'
-
+// import ProductCard from "./components/pages/ProductCard";
+import Error404 from "./components/pages/Error404";
+import Home from "./components/pages/Home";
 function App() {
-  return(
+  return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<ProductCard/>}></Route>
-        <Route path='*' element={<Error404 />}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import SubHero from "../../ui/SubHero";
+const Home = () => {
+  return (
+    <>
+      <SubHero />
+      <ProductCard />
+    </>
+  );
+};
+
+export default Home;
